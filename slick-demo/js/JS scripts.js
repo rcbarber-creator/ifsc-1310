004eb202055ea91ec59046fdf3fc335568eb3bde
rcbarber-creator <rcbarber@ualr.edu>
2023-05-05 21:34:46 +0000

    Slick Demo 7

--- slick-demo/js/JS scripts.js
+++ slick-demo/js/JS scripts.js
@@ -1,16 +1,8 @@
  
-$(document).ready(function() {
-
-    $('.fade').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      slide: 'div',
-      cssEase: 'linear',
-      autoplay: true,
-      autoplaySpeed: 2000
-    });
-
-
+$('.adaptive-height').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
 });
